2649e773b978c2050942deb61c9c7b4480b8df73
sid <68276495+siddant-rachha@users.noreply.github.com>
2023-04-24 23:45:11 +0530

    added checks of element presence

--- routes/scrapper.js
+++ routes/scrapper.js
@@ -24,19 +24,31 @@ router.post("/scrapper", async (req, res) => {
 
                 // Use Puppeteer's API to extract the product price
                 const priceElement = await page.$('div._30jeq3._16Jk6d');
-                const price = await page.evaluate(element => element.textContent, priceElement);
+                let price = "not present"
+                if (priceElement != null) {
+                    price = await page.evaluate(element => element.textContent, priceElement);
+                }
 
                 // Use Puppeteer's API to extract the product name
                 const nameElement = await page.$('span.B_NuCI');
-                const name = await page.evaluate(element => element.textContent, nameElement);
+                let name = "not present"
+                if (nameElement != null) {
+                    name = await page.evaluate(element => element.textContent, nameElement);
+                }
 
                 // Use Puppeteer's API to extract the product description
                 const descElement = await page.$('div._1mXcCf');
-                const description = await page.evaluate(element => element.textContent, descElement);
+                let description = "not present"
+                if (descElement != null) {
+                    description = await page.evaluate(element => element.textContent, descElement);
+                }
 
                 // Use Puppeteer's API to extract the product ratings
                 const ratingsElement = await page.$('div._3LWZlK');
-                const ratings = await page.evaluate(element => element.textContent, ratingsElement);
+                let ratings = "not present"
+                if (ratingsElement != null) {
+                    ratings = await page.evaluate(element => element.textContent, ratingsElement);
+                }
 
                 // Closes the browser and all of its pages
                 await browser.close();
