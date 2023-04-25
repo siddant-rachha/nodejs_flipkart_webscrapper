4e84e1872c7406bd5485f69be5927397c285c0ff
sid <68276495+siddant-rachha@users.noreply.github.com>
2023-04-25 21:42:47 +0530

    Update scrapper.js

--- routes/scrapper.js
+++ routes/scrapper.js
@@ -18,7 +18,7 @@ router.post("/scrapper", async (req, res) => {
             try {
                 // Initiate the browser
                 const browser = await puppeteer.launch({
-                    headless: 'new',
+                    headless: true,
                     args: [
                         '--no-sandbox',
                         '--disable-setuid-sandbox',
